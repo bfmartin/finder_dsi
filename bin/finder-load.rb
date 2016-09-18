e866e8e5a44b843f57f781f1b4ccc6e68dcb10c6
bfmartin <http://www.bfmartin.ca/contact>
2016-09-18 09:42:02 -0400

    added comment for how to load a table

--- bin/finder-load.rb
+++ bin/finder-load.rb
@@ -3,6 +3,9 @@
 # reads dsistrips and creates lines to be loaded into the bfmartin.ca
 # database.  See the database schema in the README.  also loads dialog
 # if available, otherwise inserts nulls.
+#
+# use a statement like this in maria db / mysql. this assumes an empty table.
+#    > load data local infile 'loadtable.txt' into table dsi;
 
 $: << File.dirname(__FILE__) + "/../lib"
 require 'finder_dsi'
